cfee06b1be0e73b66b228836b23a1dfc64cc3028
Daniel Martin <dmanlfc@gmail.com>
2025-04-20 16:43:08 +1000

    parallelisms

--- package/python-sip/python-sip.mk
+++ package/python-sip/python-sip.mk
@@ -10,7 +10,9 @@ PYTHON_SIP_SITE = https://files.pythonhosted.org/packages/62/9a/78122735697dbfc6
 PYTHON_SIP_LICENSE = SIP license or GPL-2.0 or GPL-3.0
 PYTHON_SIP_LICENSE_FILES = LICENSE LICENSE-GPL2 LICENSE-GPL3
 PYTHON_SIP_DEPENDENCIES = host-python-setuptools python3 qt5base
+PYTHON_SIP_DEPENDENCIES += host-python-setuptools-scm # batocera
 HOST_PYTHON_SIP_DEPENDENCIES = host-python3 host-python-setuptools
+HOST_PYTHON_SIP_DEPENDENCIES += host-python-setuptools-scm # batocera
 PYTHON_SIP_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
