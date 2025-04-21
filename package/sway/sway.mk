39a91ff210456c3cdb8ba756044ebcc3e21f0e97
Daniel Martin <dmanlfc@gmail.com>
2025-04-21 13:13:08 +1000

    sway & wayland updates

--- package/sway/sway.mk
+++ package/sway/sway.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # batocera (update)
-SWAY_VERSION = 1.9
+SWAY_VERSION = 1.10.1
 SWAY_SITE = https://github.com/swaywm/sway/releases/download/$(SWAY_VERSION)
 SWAY_LICENSE = MIT
 SWAY_LICENSE_FILES = LICENSE
@@ -21,11 +21,12 @@ SWAY_CONF_OPTS = \
 # batocera - We don't want systemd
 #-Dsd-bus-provider=libsystemd
 
-ifeq ($(BR2_PACKAGE_WLROOTS_X11),y)
-SWAY_CONF_OPTS += -Dxwayland=enabled
-else
-SWAY_CONF_OPTS += -Dxwayland=disabled
-endif
+# batocera - no longer an option
+#ifeq ($(BR2_PACKAGE_WLROOTS_X11),y)
+#SWAY_CONF_OPTS += -Dxwayland=enabled
+#else
+#SWAY_CONF_OPTS += -Dxwayland=disabled
+#endif
 
 ifeq ($(BR2_PACKAGE_GDK_PIXBUF),y)
 SWAY_CONF_OPTS += -Dgdk-pixbuf=enabled
