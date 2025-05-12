c6fe91d4d3ef448b6166b0b44e675855429dbbb5
Daniel Martin <dmanlfc@gmail.com>
2025-05-12 10:32:06 +1000

    llvm-project updates - improved support for newer amd gpus

--- package/llvm-project/llvm-project.mk
+++ package/llvm-project/llvm-project.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 # batocera - bump
-LLVM_PROJECT_VERSION_MAJOR = 19
-LLVM_PROJECT_VERSION = $(LLVM_PROJECT_VERSION_MAJOR).1.7
+LLVM_PROJECT_VERSION_MAJOR = 20
+LLVM_PROJECT_VERSION = $(LLVM_PROJECT_VERSION_MAJOR).1.4
 LLVM_PROJECT_SITE = https://github.com/llvm/llvm-project/releases/download/llvmorg-$(LLVM_PROJECT_VERSION)
 
 include $(sort $(wildcard package/llvm-project/*/*.mk))
