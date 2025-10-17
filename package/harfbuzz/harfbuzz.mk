e9f24257e39004f8ced81bf60b5441426fc50ebf
Daniel Martin <dmanlfc@gmail.com>
2025-10-17 21:31:29 +1000

    dependency updates for pcsx2

--- package/harfbuzz/harfbuzz.mk
+++ package/harfbuzz/harfbuzz.mk
@@ -3,8 +3,8 @@
 # harfbuzz
 #
 ################################################################################
-
-HARFBUZZ_VERSION = 10.0.1
+# batocera - bump
+HARFBUZZ_VERSION = 12.0.0
 HARFBUZZ_SITE = https://github.com/harfbuzz/harfbuzz/releases/download/$(HARFBUZZ_VERSION)
 HARFBUZZ_SOURCE = harfbuzz-$(HARFBUZZ_VERSION).tar.xz
 HARFBUZZ_LICENSE = MIT, ISC (ucdn library)
